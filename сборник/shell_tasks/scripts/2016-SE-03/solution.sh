ff92a615cb57f3903e224efe719eae520ef64ea8
Irena Stancheva <147410058+ren-rens@users.noreply.github.com>
2025-08-18 13:08:35 +0300

    Create solution.sh

---
+++ сборник/shell_tasks/scripts/2016-SE-03/solution.sh
@@ -0,0 +1,21 @@
+!/bin/bash
+if [[ "${USER}" != "root" ]]; then
+    echo "not root!"
+    exit 1
+fi
+  
+
+if [[ $# != 1 ]] || [[ ! -f "${1}" ]]; then
+    echo "one file argument needed!"
+    exit 2
+fi
+ 
+while read line; do
+    homedir=$(echo "${line}" | cut -d ':' -f 6)
+    user=$(echo "${line}" | cut -d ':' -f 1)
+    if [[ "${homedir}" == "" ]]; then
+        echo "${user} \n"
+    fi
+done< <(cat "${1}")
+
+exit 0
